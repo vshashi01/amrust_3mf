fe82331438c0151932c49a39844b5793186b2a78
Shashi <vshashi01@gmail.com>
2025-09-09 08:04:02 +0200

    custom serde of vertices

--- benches/reader.rs
+++ benches/reader.rs
@@ -6,15 +6,11 @@ use amrust_3mf::core::model::Model;
 
 use std::path::PathBuf;
 
-
 pub fn read_instant_xml(c: &mut Criterion) {
     let path = PathBuf::from("benches/3dmodel.model")
         .canonicalize()
         .unwrap();
-    let text = std::fs::read_to_string(
-        path,
-    )
-    .unwrap();
+    let text = std::fs::read_to_string(path).unwrap();
     let mut c = c.benchmark_group("read_group");
     c.sample_size(10);
     c.measurement_time(std::time::Duration::from_secs(70));
