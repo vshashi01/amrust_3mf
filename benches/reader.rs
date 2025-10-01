b3ec07a5ae7d1aff5112a5e39e37e22e1cdf8218
Shashi <vshashi01@gmail.com>
2025-10-01 11:24:59 +0200

    Added basic speedup for FromXML

--- benches/reader.rs
+++ benches/reader.rs
@@ -1,21 +1,140 @@
 use criterion::{Criterion, criterion_group, criterion_main};
-use instant_xml::from_str;
-use roxmltree::Document;
+// use lib3mf_rs::*;
+// use roxmltree::Document;
 
-use amrust_3mf::core::model::Model;
+use amrust_3mf::io::ThreemfPackage;
 
 use std::path::PathBuf;
 
-pub fn read_instant_xml(c: &mut Criterion) {
-    let path = PathBuf::from("benches/3dmodel.model")
+fn amrust_3mf_reader(
+    path: &PathBuf,
+    expected_mesh_count: usize,
+    expected_triangle_indices_count: usize,
+    expected_composed_part_count: usize,
+    expected_build_items_count: usize,
+) {
+    let reader = std::fs::File::open(path).unwrap();
+    let package = ThreemfPackage::from_reader(reader, true).unwrap();
+    let mut number_of_objects = 0;
+    let mut number_of_triangles = 0;
+    let mut number_of_components = 0;
+    package.root.resources.object.iter().for_each(|o| {
+        if let Some(mesh) = &o.mesh {
+            number_of_objects += 1;
+            number_of_triangles += mesh.triangles.triangle.len();
+        } else if o.components.is_some() {
+            number_of_components += 1;
+        }
+    });
+    if number_of_objects != expected_mesh_count {
+        panic!("Number of objects did not match");
+    }
+
+    if number_of_triangles != expected_triangle_indices_count {
+        //println!("Number of triangles is: {number_of_triangles}");
+        panic!("Number of triangle indices did not match");
+    }
+
+    if number_of_components != expected_composed_part_count {
+        panic!("Number of Composed parts did not match");
+    }
+
+    if package.root.build.item.len() != expected_build_items_count {
+        panic!("Number of Build items did not match");
+    }
+}
+
+// fn panic_if_not_success(result: Lib3MFResult, reason: &str) {
+//     if result != 0 {
+//         panic!("{}", reason);
+//     }
+// }
+
+// fn lib3mf_rs_read_model(
+//     file: &Path,
+//     expected_number_of_objects: u64,
+//     expected_number_of_triangles: u64,
+// ) {
+//     let mut model: lib3mf_rs::Lib3MF_Model = std::ptr::null_mut();
+//     let res = lib3mf_rs::get_3mf_model_from_file(file.to_path_buf(), &mut model);
+//     panic_if_not_success(res, "Model not loaded");
+
+//     let mut number_of_objects: lib3mf_rs::Lib3MF_uint64 = 0;
+//     let mut number_of_triangles = 0;
+//     unsafe {
+//         let mut mesh_obj_it: lib3mf_rs::Lib3MF_MeshObjectIterator = std::ptr::null_mut();
+//         let res = lib3mf_model_getmeshobjects(model, &mut mesh_obj_it);
+//         panic_if_not_success(res, "Get Mesh objects failed");
+
+//         let res = lib3mf_resourceiterator_count(mesh_obj_it, &mut number_of_objects);
+//         panic_if_not_success(res, "Get objects count failed");
+
+//         let mut has_next = false;
+//         loop {
+//             let res = lib3mf_resourceiterator_movenext(mesh_obj_it, &mut has_next);
+//             panic_if_not_success(res, "Failed to move the iterator");
+//             if !has_next {
+//                 break;
+//             }
+
+//             let mut object: Lib3MF_Object = std::ptr::null_mut();
+//             let res = lib3mf_meshobjectiterator_getcurrentmeshobject(mesh_obj_it, &mut object);
+//             panic_if_not_success(res, "Mesh object not acquired");
+
+//             let mut indices_buffer = Vec::with_capacity(100);
+//             let mut needed_buffer_length: Lib3MF_uint64 = 0;
+//             let res = lib3mf_meshobject_gettriangleindices(
+//                 object,
+//                 100,
+//                 &mut needed_buffer_length,
+//                 indices_buffer.as_mut_ptr(),
+//             );
+//             panic_if_not_success(res, "Getting triangles indices failed");
+//             number_of_triangles += needed_buffer_length;
+
+//             lib3mf_release(object);
+//         }
+
+//         lib3mf_release(mesh_obj_it);
+//     }
+
+//     if number_of_objects != expected_number_of_objects {
+//         panic!("Number of objects did not match");
+//     }
+
+//     if number_of_triangles != expected_number_of_triangles {
+//         // println!("Number of triangles is: {number_of_triangles}");
+//         panic!("Number of triangles did not match");
+//     }
+
+//     unsafe {
+//         lib3mf_release(model);
+//     }
+// }
+
+// pub fn read_lib3mf_rs(c: &mut Criterion) {
+//     let path = PathBuf::from("tests/data/third-party/mgx-iron_giant_single.3mf")
+//         .canonicalize()
+//         .unwrap();
+
+//     let mut c = c.benchmark_group("read_group");
+//     c.sample_size(10);
+//     c.measurement_time(std::time::Duration::from_secs(70));
+//     c.bench_function("Lib3mf Reader", |b| {
+//         b.iter(|| lib3mf_rs_read_model(&path, 1, 1204183))
+//     });
+// }
+
+pub fn reader_amrust_3mf(c: &mut Criterion) {
+    let path = PathBuf::from("tests/data/third-party/mgx-iron_giant_single.3mf")
         .canonicalize()
         .unwrap();
-    let text = std::fs::read_to_string(path).unwrap();
+
     let mut c = c.benchmark_group("read_group");
     c.sample_size(10);
     c.measurement_time(std::time::Duration::from_secs(70));
-    c.bench_function("read instant_xml function", |b| {
-        b.iter(|| from_str::<Model>(&text).unwrap())
+    c.bench_function("ThreemfPackage reader", |b| {
+        b.iter(|| amrust_3mf_reader(&path, 1, 1204183, 0, 1))
     });
 }
 
@@ -37,5 +156,5 @@ pub fn read_instant_xml(c: &mut Criterion) {
 //     });
 // }
 
-criterion_group!(benches, read_instant_xml);
+criterion_group!(benches, reader_amrust_3mf /* read_lib3mf_rs */,);
 criterion_main!(benches);
--- src/core/mesh.rs
+++ src/core/mesh.rs
@@ -1,10 +1,9 @@
-use std::collections::HashMap;
-
 use instant_xml::*;
-use regex::Regex;
 
 use crate::core::triangle_set::TriangleSets;
 use crate::threemf_namespaces::{CORE_NS, CORE_TRIANGLESET_NS};
+
+use std::f64;
 
 /// A triangle mesh
 ///
@@ -35,11 +34,17 @@ pub struct Mesh {
 }
 
 /// A list of vertices, as a struct mainly to comply with easier serde xml
-#[derive(PartialEq, Clone, Debug)]
+#[cfg_attr(feature = "speedup", derive(ToXml, PartialEq, Clone, Debug))]
+#[cfg_attr(
+    not(feature = "speedup"),
+    derive(FromXml, ToXml, PartialEq, Clone, Debug)
+)]
+#[xml(ns(CORE_NS), rename = "vertices")]
 pub struct Vertices {
     pub vertex: Vec<Vertex>,
 }
 
+#[cfg(feature = "speedup")]
 impl<'xml> FromXml<'xml> for Vertices {
     fn matches(id: Id<'_>, _field: Option<Id<'_>>) -> bool {
         id.name == "vertices" && id.ns == CORE_NS
@@ -63,6 +68,7 @@ impl<'xml> FromXml<'xml> for Vertices {
                 //println!("This is element value {:?}", element);
                 let mut vertex_value: Option<Vertex> = None;
                 let mut nested = deserializer.nested(element);
+
                 if Vertex::deserialize(&mut vertex_value, "vertex", &mut nested).is_ok()
                     && let Some(vertex) = vertex_value
                 {
@@ -78,33 +84,36 @@ impl<'xml> FromXml<'xml> for Vertices {
     }
 
     type Accumulator = Option<Self>;
-
     const KIND: Kind = Kind::Scalar;
 }
 
-impl ToXml for Vertices {
-    fn serialize<W: std::fmt::Write + ?Sized>(
-        &self,
-        field: Option<Id<'_>>,
-        serializer: &mut Serializer<W>,
-    ) -> Result<(), Error> {
-        serializer.write_start("vertices", CORE_NS)?;
-        serializer.end_start()?;
+// impl ToXml for Vertices {
+//     fn serialize<W: std::fmt::Write + ?Sized>(
+//         &self,
+//         field: Option<Id<'_>>,
+//         serializer: &mut Serializer<W>,
+//     ) -> Result<(), Error> {
+//         serializer.write_start("vertices", CORE_NS)?;
+//         serializer.end_start()?;
 
-        self.vertex.iter().for_each(|v| {
-            let _ = serializer.write_str(&format!(
-                "<vertex x=\"{}\" y=\"{}\" z=\"{}\" />",
-                v.x, v.y, v.z
-            ));
-        });
-        serializer.write_close(None, "vertices")?;
+//         self.vertex.iter().for_each(|v| {
+//             let _ = serializer.write_str(&format!(
+//                 "<vertex x=\"{}\" y=\"{}\" z=\"{}\" />",
+//                 v.x, v.y, v.z
+//             ));
+//         });
+//         serializer.write_close(None, "vertices")?;
 
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }
 
 /// A vertex in a triangle mesh
-#[derive(FromXml, ToXml, PartialEq, Clone, Debug)]
+#[cfg_attr(feature = "speedup", derive(ToXml, PartialEq, Clone, Debug))]
+#[cfg_attr(
+    not(feature = "speedup"),
+    derive(FromXml, ToXml, PartialEq, Clone, Debug)
+)]
 #[xml(ns(CORE_NS), rename = "vertex")]
 pub struct Vertex {
     #[xml(attribute)]
@@ -117,46 +126,184 @@ pub struct Vertex {
     pub z: f64,
 }
 
-// impl<'xml> FromXml<'xml> for Vertex {
-//     fn matches(id: Id<'_>, field: Option<Id<'_>>) -> bool {
-//         match field {
-//             Some(field) => field == id,
-//             None => id.ns == CORE_NS && id.name == "vertex",
-//         }
-//         // true
-//     }
+#[cfg(feature = "speedup")]
+impl<'xml> FromXml<'xml> for Vertex {
+    #[inline]
+    fn matches(id: ::instant_xml::Id<'_>, _: Option<::instant_xml::Id<'_>>) -> bool {
+        id == ::instant_xml::Id {
+            ns: CORE_NS,
+            name: "vertex",
+        }
+    }
+    fn deserialize<'cx>(
+        into: &mut Self::Accumulator,
+        _: &'static str,
+        deserializer: &mut ::instant_xml::Deserializer<'cx, 'xml>,
+    ) -> ::std::result::Result<(), ::instant_xml::Error> {
+        use ::instant_xml::Error;
+        use ::instant_xml::de::Node;
+        let mut x: f64 = 0.0;
+        let mut y: f64 = 0.0;
+        let mut z: f64 = 0.0;
+        while let Some(node) = deserializer.next() {
+            let node = node?;
+            match node {
+                Node::Attribute(attr) => {
+                    let id = deserializer.attribute_id(&attr)?;
+                    // println!("Attr value: {:?}", attr.value);
+                    match id.name {
+                        "x" => {
+                            x = attr.value.parse().unwrap_or_default();
+                        }
 
+                        "y" => {
+                            y = attr.value.parse().unwrap_or_default();
+                        }
+                        "z" => {
+                            z = attr.value.parse().unwrap_or_default();
+                        }
+                        _ => {
+                            let mut nested =
+                                deserializer.for_node(Node::AttributeValue(attr.value));
+                            nested.ignore()?;
+                        }
+                    }
+                }
+                Node::Open(data) => {
+                    let mut nested = deserializer.nested(data);
+                    nested.ignore()?;
+                }
+                Node::Text(_) => {}
+                _ => {
+                    return Err(Error::UnexpectedNode("Unexpected".to_owned()));
+                }
+            }
+        }
+        *into = Some(Self { x, y, z });
+        Ok(())
+    }
+    type Accumulator = Option<Self>;
+    const KIND: ::instant_xml::Kind = ::instant_xml::Kind::Element;
+}
+
+// impl<'xml> FromXml<'xml> for Vertex {
+//     #[inline]
+//     fn matches(id: ::instant_xml::Id<'_>, field: Option<::instant_xml::Id<'_>>) -> bool {
+//         id == ::instant_xml::Id {
+//             ns: CORE_NS,
+//             name: "vertex",
+//         }
+//     }
 //     fn deserialize<'cx>(
 //         into: &mut Self::Accumulator,
 //         field: &'static str,
-//         deserializer: &mut Deserializer<'cx, 'xml>,
-//     ) -> Result<(), Error> {
-//         if into.is_some() {
-//             return Err(Error::DuplicateValue("Vertex already exists"));
+//         deserializer: &mut ::instant_xml::Deserializer<'cx, 'xml>,
+//     ) -> ::std::result::Result<(), ::instant_xml::Error> {
+//         use ::instant_xml::de::Node;
+//         use ::instant_xml::{Accumulate, Error, FromXml, Id, Kind};
+//         enum __Elements {
+//             __Ignore,
 //         }
-
-//         let value = match deserializer.take_str()? {
-//             Some(value) => value,
-//             None => return Err(Error::MissingValue("Missing vertex attributes")),
-//         };
-
-//         println!("The value is {value:?}");
-
+//         enum __Attributes {
+//             __Value0,
+//             __Value1,
+//             __Value2,
+//             __Ignore,
+//         }
+//         let mut __value0 = <f64 as FromXml>::Accumulator::default();
+//         let mut __value1 = <f64 as FromXml>::Accumulator::default();
+//         let mut __value2 = <f64 as FromXml>::Accumulator::default();
+//         loop {
+//             let node = match deserializer.next() {
+//                 Some(result) => result?,
+//                 None => break,
+//             };
+//             match node {
+//                 Node::Attribute(attr) => {
+//                     let id = deserializer.attribute_id(&attr)?;
+//                     println!("Attribute value: {}", attr.value);
+//                     let field = if <f64 as FromXml>::matches(id, Some(Id { ns: "", name: "x" })) {
+//                         __Attributes::__Value0
+//                     } else if <f64 as FromXml>::matches(id, Some(Id { ns: "", name: "y" })) {
+//                         __Attributes::__Value1
+//                     } else if <f64 as FromXml>::matches(id, Some(Id { ns: "", name: "z" })) {
+//                         __Attributes::__Value2
+//                     } else {
+//                         __Attributes::__Ignore
+//                     };
+//                     match field {
+//                         __Attributes::__Value0 => {
+//                             let mut nested =
+//                                 deserializer.for_node(Node::AttributeValue(attr.value));
+//                             let new = <f64 as FromXml>::deserialize(
+//                                 &mut __value0,
+//                                 "Vertex::x",
+//                                 &mut nested,
+//                             )?;
+//                         }
+//                         __Attributes::__Value1 => {
+//                             let mut nested =
+//                                 deserializer.for_node(Node::AttributeValue(attr.value));
+//                             let new = <f64 as FromXml>::deserialize(
+//                                 &mut __value1,
+//                                 "Vertex::y",
+//                                 &mut nested,
+//                             )?;
+//                         }
+//                         __Attributes::__Value2 => {
+//                             let mut nested =
+//                                 deserializer.for_node(Node::AttributeValue(attr.value));
+//                             let new = <f64 as FromXml>::deserialize(
+//                                 &mut __value2,
+//                                 "Vertex::z",
+//                                 &mut nested,
+//                             )?;
+//                         }
+//                         __Attributes::__Ignore => {}
+//                     }
+//                 }
+//                 Node::Open(data) => {
+//                     let id = deserializer.element_id(&data)?;
+//                     let element = __Elements::__Ignore;
+//                     match element {
+//                         __Elements::__Ignore => {
+//                             let mut nested = deserializer.nested(data);
+//                             nested.ignore()?;
+//                         }
+//                     }
+//                 }
+//                 Node::Text(_) => {}
+//                 node => {
+//                     // return Err(Error::UnexpectedNode(::alloc::__export::must_use({
+//                     //     ::alloc::fmt::format(format_args!("{0:?} in {1}", node, "Vertex"))
+//                     // })));
+//                     return Err(Error::UnexpectedNode("Unexpected".to_owned()));
+//                 }
+//             }
+//         }
+//         *into = Some(Self {
+//             x: __value0.try_done("Vertex::x")?,
+//             y: __value1.try_done("Vertex::y")?,
+//             z: __value2.try_done("Vertex::z")?,
+//         });
 //         Ok(())
 //     }
-
 //     type Accumulator = Option<Self>;
-
-//     const KIND: Kind = Kind::Scalar;
+//     const KIND: ::instant_xml::Kind = ::instant_xml::Kind::Element;
 // }
 
 /// A list of triangles, as a struct mainly to comply with easier serde xml
-#[derive(PartialEq, Clone, Debug)]
-//#[xml(ns(CORE_NS), rename = "triangles")]
+#[cfg_attr(feature = "speedup", derive(ToXml, PartialEq, Clone, Debug))]
+#[cfg_attr(
+    not(feature = "speedup"),
+    derive(FromXml, ToXml, PartialEq, Clone, Debug)
+)]
+#[xml(ns(CORE_NS), rename = "triangles")]
 pub struct Triangles {
     pub triangle: Vec<Triangle>,
 }
 
+#[cfg(feature = "speedup")]
 impl<'xml> FromXml<'xml> for Triangles {
     fn matches(id: Id<'_>, _field: Option<Id<'_>>) -> bool {
         id.name == "triangles" && id.ns == CORE_NS
@@ -176,7 +323,6 @@ impl<'xml> FromXml<'xml> for Triangles {
             if let Ok(n) = node
                 && let de::Node::Open(element) = n
             {
-                //println!("This is element value {:?}", element);
                 let mut triangle_value: Option<Triangle> = None;
                 let mut nested = deserializer.nested(element);
                 if Triangle::deserialize(&mut triangle_value, field, &mut nested).is_ok()
@@ -200,34 +346,39 @@ impl<'xml> FromXml<'xml> for Triangles {
     const KIND: Kind = Kind::Element;
 }
 
-impl ToXml for Triangles {
-    fn serialize<W: std::fmt::Write + ?Sized>(
-        &self,
-        field: Option<Id<'_>>,
-        serializer: &mut Serializer<W>,
-    ) -> Result<(), Error> {
-        serializer.write_start("triangles", CORE_NS)?;
-        serializer.end_start()?;
+// impl ToXml for Triangles {
+//     fn serialize<W: std::fmt::Write + ?Sized>(
+//         &self,
+//         field: Option<Id<'_>>,
+//         serializer: &mut Serializer<W>,
+//     ) -> Result<(), Error> {
+//         serializer.write_start("triangles", CORE_NS)?;
+//         serializer.end_start()?;
 
-        self.triangle.iter().for_each(|v| {
-            //let _ = v.serialize(Some(Id { ns: CORE_NS, name: "triangle" }), serializer);
-            //let _ = v.serialize(None, serializer);
-            let _ = serializer.write_str(&format!(
-                "<triangle v1=\"{}\" v2=\"{}\" v3=\"{}\" />",
-                v.v1, v.v2, v.v3
-            ));
-        });
-        serializer.write_close(None, "triangles")?;
+//         self.triangle.iter().for_each(|v| {
+//             //let _ = v.serialize(Some(Id { ns: CORE_NS, name: "triangle" }), serializer);
+//             //let _ = v.serialize(None, serializer);
+//             let _ = serializer.write_str(&format!(
+//                 "<triangle v1=\"{}\" v2=\"{}\" v3=\"{}\" />",
+//                 v.v1, v.v2, v.v3
+//             ));
+//         });
+//         serializer.write_close(None, "triangles")?;
 
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }
 
 /// A triangle in a triangle mesh
 ///
 /// The triangle consists of indices that refer to the vertices of the mesh. See
 /// [`TriangleMesh`].
-#[derive(FromXml, ToXml, PartialEq, Clone, Debug)]
+#[cfg_attr(feature = "speedup", derive(ToXml, PartialEq, Clone, Debug))]
+#[cfg_attr(
+    not(feature = "speedup"),
+    derive(FromXml, ToXml, PartialEq, Clone, Debug)
+)]
+//#[derive(FromXml, ToXml, PartialEq, Clone, Debug)]
 #[xml(ns(CORE_NS), rename = "triangle")]
 pub struct Triangle {
     #[xml(attribute)]
@@ -250,6 +401,100 @@ pub struct Triangle {
 
     #[xml(attribute)]
     pub pid: Option<usize>,
+}
+
+#[cfg(feature = "speedup")]
+impl<'xml> FromXml<'xml> for Triangle {
+    #[inline]
+    fn matches(id: ::instant_xml::Id<'_>, _: Option<::instant_xml::Id<'_>>) -> bool {
+        id == ::instant_xml::Id {
+            ns: CORE_NS,
+            name: "triangle",
+        }
+    }
+    fn deserialize<'cx>(
+        into: &mut Self::Accumulator,
+        _: &'static str,
+        deserializer: &mut ::instant_xml::Deserializer<'cx, 'xml>,
+    ) -> ::std::result::Result<(), ::instant_xml::Error> {
+        use ::instant_xml::Error;
+        use ::instant_xml::de::Node;
+        let mut v1: usize = 0;
+        let mut v2: usize = 0;
+        let mut v3: usize = 0;
+        let mut p1: Option<usize> = None;
+        let mut p2: Option<usize> = None;
+        let mut p3: Option<usize> = None;
+        let mut pid: Option<usize> = None;
+
+        while let Some(node) = deserializer.next() {
+            let node = node?;
+            match node {
+                Node::Attribute(attr) => {
+                    let id = deserializer.attribute_id(&attr)?;
+                    // println!("Attr value: {:?}", attr.value);
+                    match id.name {
+                        "v1" => {
+                            v1 = attr.value.parse().unwrap_or_default();
+                        }
+
+                        "v2" => {
+                            v2 = attr.value.parse().unwrap_or_default();
+                        }
+                        "v3" => {
+                            v3 = attr.value.parse().unwrap_or_default();
+                        }
+                        "p1" => {
+                            if let Ok(value) = attr.value.parse::<usize>() {
+                                p1 = Some(value);
+                            }
+                        }
+                        "p2" => {
+                            if let Ok(value) = attr.value.parse::<usize>() {
+                                p2 = Some(value);
+                            }
+                        }
+                        "p3" => {
+                            if let Ok(value) = attr.value.parse::<usize>() {
+                                p3 = Some(value);
+                            }
+                        }
+                        "pid" => {
+                            if let Ok(value) = attr.value.parse::<usize>() {
+                                pid = Some(value);
+                            }
+                        }
+
+                        _ => {
+                            let mut nested =
+                                deserializer.for_node(Node::AttributeValue(attr.value));
+                            nested.ignore()?;
+                        }
+                    }
+                }
+                Node::Open(data) => {
+                    let mut nested = deserializer.nested(data);
+                    nested.ignore()?;
+                }
+                Node::Text(_) => {}
+                _ => {
+                    return Err(Error::UnexpectedNode("Unexpected".to_owned()));
+                }
+            }
+        }
+        *into = Some(Self {
+            v1,
+            v2,
+            v3,
+            p1,
+            p2,
+            p3,
+            pid,
+        });
+        Ok(())
+    }
+    type Accumulator = Option<Self>;
+    const KIND: ::instant_xml::Kind = ::instant_xml::Kind::Element;
 }
 
 #[cfg(test)]
