a239a9964f145e060e533f41e76d4a0f71e9c77e
Shashi <vshashi01@gmail.com>
2025-11-28 15:38:32 +0100

    Add docs for core modules

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,11 +1,29 @@
-pub mod build;
-pub mod component;
-pub mod mesh;
-pub mod metadata;
+/// Defines the type representing the 3MF Model element and elements and attributes related to it.
 pub mod model;
-pub mod object;
+
+/// Defines the type representing the Metadata element and other elements and attributes related to it.
+pub mod metadata;
+
+/// Defines the type representing the Resources elements and other elements and attributes related to it.
 pub mod resources;
+
+/// Defines the type representing a  Object and other elements and atrributes related to it.
+pub mod object;
+
+/// Defines the type representing a Mesh object and other elements and attributes related to it.
+pub mod mesh;
+
+/// Defines the type representing a Components object and other elements and attributes related to it.
+pub mod component;
+
+/// Defines the Transform type for use in Model.
 pub mod transform;
+
+/// Defines the type representing Build and other elements and attributes related to it.
+pub mod build;
+
+/// Defines the type representing Triangle Sets and other elements and attributes related to it.
 pub mod triangle_set;
 
+/// Defines the type representing a Beam Lattice element and other elements and attributes related to it.
 pub mod beamlattice;
